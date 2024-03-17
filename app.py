4dca011fc084c0d11cfc37721327d004996b2bec
ShrewdLuni <ander113q@gmail.com>
2024-03-17 20:31:19 +0200

    feat: updated detector

--- app.py
+++ app.py
@@ -1,50 +1,53 @@
 import math
+import pickle
 import time
 
 import cv2
+import numpy
 import mediapipe as mp
 
-# Initialize MediaPipe Hands module
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 
-# Initialize MediaPipe Drawing module for drawing landmarks
 mp_drawing = mp.solutions.drawing_utils
 
-# Open a video capture object (0 for the default camera)
 cap = cv2.VideoCapture(0)
 end = 0
+
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
+
+labels_dict = {0: 'A', 1: 'B', 2: 'C'}
+
 while cap.isOpened():
     start = time.time()
     ret, frame = cap.read()
     fps = math.ceil(1 / (time.time()-end))
     end = start
 
-    if not ret:
-        continue
+    data = []
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    results = hands.process(frame_rgb)
-
     image_height, image_width, _ = frame_rgb.shape
 
+    results = hands.process(frame_rgb)
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            for ids, landmrk in enumerate(hand_landmarks.landmark):
-                # print(ids, landmrk)
-                cx, cy = landmrk.x * image_width, landmrk.y * image_height
-                print (ids, cx, cy)
-            # Draw landmarks on the frame
-            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,landmark_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(color=(0, 255, 0), thickness=4, circle_radius=2),connection_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(color=(20, 180, 90), thickness=2, circle_radius=2))
+            for i in range(len(hand_landmarks.landmark)):
+                data.append(hand_landmarks.landmark[i].x)
+                data.append(hand_landmarks.landmark[i].y)
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,landmark_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(color=(255, 255, 0), thickness=4, circle_radius=2),connection_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(color=(0, 0, 0), thickness=2, circle_radius=2))
 
-    # Display the frame with hand landmarks
+        prediction = labels_dict[int(model.predict([numpy.asarray(data)])[0])]
+
+        print(prediction)
+
+
     cv2.putText(frame,str(fps),(10,30),cv2.FONT_HERSHEY_SIMPLEX ,1,(0,0,255),2 )
-    cv2.imshow('Hand Recognition', frame)
 
-    # Exit when 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv2.imshow('Hand Recognition', frame)
+    cv2.waitKey(1)
 
 # Release the video capture object and close the OpenCV windows
 cap.release()
